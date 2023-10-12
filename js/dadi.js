0c37bb838566157217ccaf0915b02ef8009b5dfa
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-12 16:09:05 +0200

    number-generator-set

---
+++ js/dadi.js
@@ -0,0 +1,7 @@
+
+let randomNumberPlayer = Math.floor(Math.random() * 6) + 1;
+let randomNumberComputer = Math.floor(Math.random() * 6) + 1;
+
+console.log(randomNumberPlayer);
+console.log(randomNumberComputer);
+
