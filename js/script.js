07e1fce34a78c4654d3036ff92940adbbd20ea1f
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-12 14:58:28 +0200

    input-email-set-html-js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const mailList =
+[
+    'Giulia@gmail.com',
+    'Erica@gmail.com',
+    'Matteo@gmail.com',
+    'Alexandru@gmail.com',
+    'mekki@gmail.com',
+    'Simone@gmail.com'
+];
+
+
+// valore input email
+let mailValue = document.getElementById('mail').value;
+let search = false;
+
+console.log(mailValue);
+
+
