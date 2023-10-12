adb4c851a6ec9f6147195158b7062e34036bd54b
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-12 23:16:33 +0200

    outcome-set

--- js/dadi.js
+++ js/dadi.js
@@ -5,29 +5,37 @@ const diceEl = document.querySelectorAll('.dice');
 button.addEventListener('click',
 function()
 {
-    
     diceEl.forEach((e) =>
-        {
-            e.classList.add('d-none')
-        });
+    {
+        e.classList.add('d-none')
+    });
 
     let randomNumberPlayer = Math.floor(Math.random() * 6) + 1;
     let randomNumberComputer = Math.floor(Math.random() * 6) + 1;
+    let playerOutcome = document.querySelector('.player .outcome')
+    let computerOutcome = document.querySelector('.computer .outcome')
         
     if(randomNumberComputer > randomNumberPlayer)
     {
-        console.log('Computer wins')
+        console.log('Computer wins');
+        playerOutcome.innerText = 'Loser';
+        computerOutcome.innerText = 'Winner';
     }
     
     else if(randomNumberComputer < randomNumberPlayer)
     {
-        console.log('Player wins')
+        console.log('Player wins');
+        playerOutcome.innerText = 'Winner';
+        computerOutcome.innerText = 'Loser';
+
     }
     
     else 
     {
-        console.log('draw')
+        console.log('draw');
+        playerOutcome.innerText = computerOutcome.innerText = 'Draw';
     }
+
 
     console.log('Player ' + randomNumberPlayer);
     console.log('Computer ' + randomNumberComputer);
