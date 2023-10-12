8a11f48bc83409ff3463d5c9eabac854b740575c
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-12 16:18:27 +0200

    dadi-operation-done

--- js/dadi.js
+++ js/dadi.js
@@ -2,6 +2,20 @@
 let randomNumberPlayer = Math.floor(Math.random() * 6) + 1;
 let randomNumberComputer = Math.floor(Math.random() * 6) + 1;
 
-console.log(randomNumberPlayer);
-console.log(randomNumberComputer);
+console.log('Player ' + randomNumberPlayer);
+console.log('Computer ' + randomNumberComputer);
 
+if(randomNumberComputer > randomNumberPlayer)
+{
+    console.log('Computer wins')
+}
+
+else if(randomNumberComputer < randomNumberPlayer)
+{
+    console.log('Player wins')
+}
+
+else 
+{
+    console.log('draw')
+}
